394f2b8738a0c704394d1ad8e7eafc69d785229c
mspegagne <mathieu.sge@gmail.com>
2015-03-08 16:35:46 +0530

    first commit

---
+++ src/com/Ms/ToDoReminder/MainActivity.java
@@ -0,0 +1,20 @@
+package com.Ms.ToDoReminder;
+
+/**
+ * @author SPEGAGNE Mathieu on 08/03/15.
+ * @author https://github.com/mspegagne
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
