f4603ecef702ab3d6c9ab3c5ef34b0d4f22bb779
mspegagne <mathieu.sge@gmail.com>
2015-03-13 13:16:27 +0530

    datePicker

--- ToDoReminder/src/main/java/com/Ms/todoreminder/AddFragment.java
+++ ToDoReminder/src/main/java/com/Ms/todoreminder/AddFragment.java
@@ -4,18 +4,60 @@ package com.Ms.todoreminder;
  * @author SPEGAGNE Mathieu on 11/03/15.
  * @author https://github.com/mspegagne
  */
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AddFragment extends Fragment {
+
+    EditText txtDate;
+    private int mYear, mMonth, mDay;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View AddView =  inflater.inflate(R.layout.fragment_add, container, false);
+
+        txtDate = (EditText)AddView.findViewById(R.id.addDate);
+        txtDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Process to get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                // Launch Date Picker Dialog
+                DatePickerDialog dpd = new DatePickerDialog(getView().getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(android.widget.DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                // Display Selected date in textbox
+                                txtDate.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+
+
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+            }
+        });
+
+
+        return AddView;
     }
+
+
 }
